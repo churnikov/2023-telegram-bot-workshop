6e06d7eb336a9e4fc4f45d3278764eb813c7fcec
Nikita Churikov <nikita@chur.ru>
2023-11-06 22:47:31 +0100

    added getting updates

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,8 +1,22 @@
 import os
+import asyncio
 
 from dotenv import load_dotenv
+import aiohttp
 
 
 load_dotenv()
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.telegram.org/bot{}/getMe'.format(TG_BOT_TOKEN)) as resp:
+            print(resp.status)
+            print(await resp.json())
+
+        async with session.get('https://api.telegram.org/bot{}/getUpdates'.format(TG_BOT_TOKEN)) as resp:
+            print(resp.status)
+            print(await resp.json())
+
+asyncio.run(main())
