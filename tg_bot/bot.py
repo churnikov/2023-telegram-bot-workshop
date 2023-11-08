9ebabcb4a9640dc619fa17d14b776e33e48dff2e
Nikita Churikov <nikita@chur.ru>
2023-11-08 18:21:12 +0100

    added inline buttons

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -15,34 +15,16 @@ from os import getenv
 
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
+from aiogram import F
 
 # Bot token can be obtained via https://t.me/BotFather
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
-
-class Game:
-    def __init__(self):
-        self.number = random.randint(1, 100)
-        self.guesses = 0
-        self.max_guesses = 10
-    def guess(self, number):
-        if self.guesses >= self.max_guesses:
-            return f"You have no more guesses left. The number was {self.number}"
-        self.guesses += 1
-        if number == self.number:
-            return "You guessed it!"
-        elif number < self.number:
-            return "Too low"
-        else:
-            return "Too high"
-    def reset(self):
-        self.__init__()
-
-db = {}
 
 
 @dp.message(CommandStart())
@@ -56,28 +38,69 @@ async def command_start_handler(message: Message) -> None:
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-    db[message.from_user.id] = Game()
 
 
-@dp.message()
-async def handle_guess(message: types.Message) -> None:
+class MyCallback(CallbackData, prefix="my"):
+    press_data: str
+
+
+@dp.message(Command("buttons"))
+async def command_buttons_handler(message: Message) -> None:
     """
-    Handler will forward receive a message back to the sender
+    This handler receives messages with `/buttons` command
 
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    https://docs.aiogram.dev/en/dev-3.x/dispatcher/filters/callback_data.html
     """
-    if message.from_user.id not in db:
-        await message.answer("Start a game first")
-        return
-    game = db[message.from_user.id]
-    try:
-        guess = int(message.text)
-    except ValueError:
-        await message.answer("Please, enter a number")
-        return
-    await message.answer(game.guess(guess))
-    if game.guesses >= game.max_guesses:
-        del db[message.from_user.id]
+    # CallbackData is a helper class to create CallbackQuery data
+
+    cb_press = MyCallback(press_data="press")
+    cb_dont_press = MyCallback(press_data="dont_press")
+    cb_press_too = MyCallback(press_data="press_too")
+    print(cb_press.pack())
+    print(cb_dont_press.pack())
+    print(cb_press_too.pack())
+
+    await message.answer(
+        "Here are your buttons:",
+        reply_markup=types.InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    types.InlineKeyboardButton(text="Press me", callback_data=cb_press.pack()),
+                    types.InlineKeyboardButton(text="Don't press me", callback_data=cb_dont_press.pack()),
+                ],
+                [types.InlineKeyboardButton(text="Press me too", callback_data=cb_press_too.pack())],
+            ],
+        ),
+    )
+
+
+@dp.callback_query(MyCallback.filter(F.press_data=="press"))
+async def callback_press_handler(query: types.CallbackQuery, callback_data: dict[str, str]) -> None:
+    """
+    This handler receives callback queries with `press` field equal to `True`
+    """
+    await query.message.edit_text("You pressed the button!")
+    await query.answer("You pressed the button!", show_alert=True)
+
+
+@dp.callback_query(MyCallback.filter(F.press_data=="dont_press"))
+async def callback_dont_press_handler(query: types.CallbackQuery, callback_data: dict[str, str]) -> None:
+    """
+    This handler receives callback queries with `dont_press` field equal to `True`
+    """
+    await query.message.edit_text("You shouldn't have pressed that button!")
+    await query.answer("You shouldn't have pressed that button!", show_alert=True)
+
+
+@dp.callback_query(MyCallback.filter(F.press_data=="press_too"))
+async def callback_press_too_handler(query: types.CallbackQuery, callback_data: dict[str, str]) -> None:
+    """
+    This handler receives callback queries with `press_too` field equal to `True`
+    """
+    await query.message.edit_text("You pressed the button too!")
+    await query.answer("You pressed the button too!", show_alert=True)
+
+
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
